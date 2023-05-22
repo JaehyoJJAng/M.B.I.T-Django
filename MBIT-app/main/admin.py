2e7082d4b978fe696d885ea6b27e49258e02b67c
JaehyoJJang <yshrim12@naver.com>
2023-05-22 20:02:24 +0900

    관리자 페이지

--- MBIT-app/main/admin.py
+++ MBIT-app/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Developer,Question,Choice
 
 # Register your models here.
+
+
+admin.site.register(Developer)
+admin.site.register(Question)
+admin.site.register(Choice)
