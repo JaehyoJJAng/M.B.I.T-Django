e2a0132a7305b477bd7340114a77c616209f4b3d
JaehyoJJang <yshrim12@naver.com>
2023-05-23 11:42:09 +0900

    [backend] - 메인 페이지 뼈대

--- MBIT-app/main/views.py
+++ MBIT-app/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Question,Developer,Choice
 
 # Create your views here.
+
+def index(request):
+    developers = Developer.objects.all()
+    
+    context = {
+        "developers": developers,
+    }
+    return render(request=request,template_name='index.html',context=context)
