cf96d59d3aa371c069450989653f2af55f0cb10b
JaehyoJJang <yshrim12@naver.com>
2023-05-23 12:12:30 +0900

    template 작성하기 3 - 결과페이지

--- MBIT-app/main/views.py
+++ MBIT-app/main/views.py
@@ -18,3 +18,28 @@ def form(request):
         "questions": questions,
     }
     return render(request=request,template_name='form.html',context=context)
+
+def result(request):
+    # 문항 수
+    N = Question.objects.count()
+    
+    # 개발자 유형 수
+    K = Developer.objects.count()
+    
+    # 점수 누적 로직
+    counter = [0 for _ in range(K + 1)]
+    for n in range(1,N + 1):
+        developer_id = int(request.POST[f'question-{n}'][0])
+        counter[developer_id] += 1
+    
+    # 최고점 개발 유형
+    best_developer_id = max(range(1,K+1), key=lambda id : counter[id])
+    best_developer = Developer.objects.get(pk=best_developer_id)
+    best_developer.count += 1
+    best_developer.save()
+    
+    context = {
+        'developer' : best_developer,
+        'counter'   : counter
+    }    
+    return render(request=request,template_name='result.html',context=context)
